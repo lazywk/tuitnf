0dfb270ed3fb889d9ff12077824f3f8e480f6f0d
doniyoroka <edoniyorbekdev@gmail.com>
2024-03-04 23:01:35 +0500

    cxcx

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,30 @@ module.exports = {
     browser: true
   },
   parser: '@babel/eslint-parser',
+  extends: ['next/core-web-vitals', 'prettier'],
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
-    project: './jsconfig.json'
+    project: './jsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/display-name': 'off',
+    '@next/next/no-img-element': 'off',
+    'react/no-unescaped-entities': 'off',
+    'import/no-anonymous-default-export': 'off',
+
+    // add new line after each var, const, let declaration
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['export'], next: ['*'] },
+      { blankLine: 'always', prev: ['*'], next: ['multiline-const', 'multiline-let', 'multiline-var', 'export'] }
+    ]
   }
 }
